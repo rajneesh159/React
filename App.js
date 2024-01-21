d4d5c0262a4a522c7fa028c49b6608c3406907f8
Rajneesh kumar verma. <rajneeshkumarverma680@gmail.com>
2024-01-21 22:54:59 +0530

    Start is

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "I am an h1 tag"), React.createElement("h2", {}, "I am an h2 tag")]))
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
