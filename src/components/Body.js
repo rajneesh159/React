6a3ca2c8eda6dc2d2fd256f8f2e296f3cbb01e66
Rajneesh kumar verma. <rajneeshkumarverma680@gmail.com>
2024-01-24 00:38:17 +0530

    create a food orderwebsite with fiter feature

---
+++ src/components/Body.js
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import RestaurantCard from './RestaurantCard';
+import resList from '../utils/mockData';
+
+const Body = () => {
+  // * React Hook -> A normal JavaScript function which is given to us by React (or) Normal JS utility functions
+  // * useState() - Super Powerful variable
+  // * useEffect() -
+
+  // * State variable - Super Powerful variable
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
+  // * Normal JS variable
+
+
+  return (
+    <div className="body">
+
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            // * Filter logic
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+
+            setListOfRestaurants(filteredList);
+            console.log(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {/* // * looping through the <RestaurentCard /> components Using Array.map() method */}
+
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
---
+++ src/utils/contants.js
@@ -0,0 +1,5 @@
+export const CDN_URL = 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/'
+
+export const LOGO_URL = "https://i.pinimg.com/originals/12/46/56/12465647d1d0558bcc2a94d32f1d9d6b.jpg";
+
+
